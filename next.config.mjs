03fa62f48915c343dc79d2d01ef13da541dd7ec0
Fadlan Hidayatulloh <fadlanhidayatulloh@MacBook-Fadlan.local>
2026-07-30 15:53:38 +0700

    auto: sync changes [2026-07-30 15:53]

--- next.config.mjs
+++ next.config.mjs
@@ -7,13 +7,16 @@ const MIDTRANS_DOMAINS = [
 
 const VERCEL_DOMAINS = 'https://vercel.live https://*.vercel.live https://*.pusher.com wss://*.pusher.com'
 
+// Chrome / browser built-in "Translate this page" feature
+const GOOGLE_TRANSLATE_DOMAINS = 'https://translate.googleapis.com https://translate.google.com https://www.gstatic.com'
+
 const csp = [
   "default-src 'self'",
-  `script-src 'self' 'unsafe-inline' 'unsafe-eval' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS}`,
-  `style-src 'self' 'unsafe-inline' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS}`,
+  `script-src 'self' 'unsafe-inline' 'unsafe-eval' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS} ${GOOGLE_TRANSLATE_DOMAINS}`,
+  `style-src 'self' 'unsafe-inline' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS} ${GOOGLE_TRANSLATE_DOMAINS}`,
   `img-src 'self' data: blob: https:`,
   `font-src 'self' data: https:`,
-  `connect-src 'self' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS}`,
+  `connect-src 'self' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS} ${GOOGLE_TRANSLATE_DOMAINS}`,
   `frame-src 'self' ${MIDTRANS_DOMAINS} ${VERCEL_DOMAINS}`,
   `worker-src 'self' blob:`,
 ].join('; ')
